5678b27ab8d4cb360112d0ed3adbb35088cd899c
Ayush Vaish <ayushvaish.00005@gmail.com>
2026-03-29 15:20:37 +0530

    Fix Express 5 wildcard syntax for Render

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use('/api/orders', orderRoutes);
 
 // Serve Static Assets
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
 });
 
